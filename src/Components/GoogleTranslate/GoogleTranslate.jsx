a03b2421f73dd2ffb21504aff1e7eea4104e327f
makwanaom <117265350+makwanaom@users.noreply.github.com>
2024-06-28 15:08:28 +0530

    JSON data added

--- src/Components/GoogleTranslate/GoogleTranslate.jsx
+++ src/Components/GoogleTranslate/GoogleTranslate.jsx
@@ -4,30 +4,34 @@ function GoogleTranslate() {
   const [translatorLoaded, setTranslatorLoaded] = useState(false);
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://translate.google.com/translate_a/element.js';
-    script.onload = () => {
-      console.log('Google Translate script loaded.');
+    const scriptId = 'google-translate-script';
+
+    if (!document.getElementById(scriptId)) {
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.src = 'https://translate.google.com/translate_a/element.js?cb=initializeGoogleTranslate';
+      script.async = true;
+      script.defer = true;
+      script.onload = () => {
+        console.log('Google Translate script loaded.');
+        setTranslatorLoaded(true);
+      };
+      script.onerror = (e) => {
+        console.error('Error loading Google Translate script:', e);
+      };
+      document.head.appendChild(script);
+
+      window.initializeGoogleTranslate = () => {
+        if (window.google && window.google.translate && window.google.translate.TranslateElement) {
+          new window.google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_element');
+        } else {
+          console.error('Google Translate library not available.');
+        }
+      };
+    } else {
       setTranslatorLoaded(true);
-      setTimeout(initializeGoogleTranslate, 100); // Add a small delay
-    };
-    script.onerror = (e) => {
-      console.error('Error loading Google Translate script:', e);
-    };
-    document.head.appendChild(script);
-
-    const initializeGoogleTranslate = () => {
-      if (window.google && window.google.translate && window.google.translate.TranslateElement) {
-        new window.google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_element');
-      } else {
-        console.error('Google Translate library not available.');
-      }
-    };
-
-    return () => {
-     
-      document.head.removeChild(script);
-    };
+      window.initializeGoogleTranslate();
+    }
   }, []);
 
   return (
